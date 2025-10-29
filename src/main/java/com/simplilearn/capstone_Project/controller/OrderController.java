1a35943d2eef649bc06699d9ab0e4985839e152b
Dafne Lazaro <dafne.lazaro23@gmail.com>
2025-10-29 14:21:36 -0400

    Fixed an issue with incompatibility between two methods in OrderController

--- src/main/java/com/simplilearn/capstone_Project/controller/OrderController.java
+++ src/main/java/com/simplilearn/capstone_Project/controller/OrderController.java
@@ -15,14 +15,15 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    @PostMapping("/place")
-    public Order placeOrder(@RequestBody Order order) {
-        return orderService.placeOrder(order);
+    @PostMapping("/place/{userId}")
+    public Order placeOrder(@PathVariable int userId) {
+        return orderService.placeOrder(userId);
     }
 
+
     @GetMapping("/user/{userId}")
-    public List<Order> getUserOrders(@PathVariable Long userId) {
-        return orderService.getUserOrders(userId);
+    public List<Order> getUserOrders(@PathVariable int userId) {
+        return orderService.getOrdersByUser(userId);
     }
 
     @GetMapping
