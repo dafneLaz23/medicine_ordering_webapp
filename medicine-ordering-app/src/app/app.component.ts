ef49b82525ac9a128a67a684f30af71dcd30e047
Dafne Lazaro <dafne.lazaro23@gmail.com>
2025-10-30 14:52:50 -0400

    Fix incompatibility issues between app.component.ts and add-medicine.component.ts that occurred when trying to run angular

--- medicine-ordering-app/src/app/app.component.ts
+++ medicine-ordering-app/src/app/app.component.ts
@@ -1,21 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AddMedicine } from './components/admin/add-medicine/add-medicine.component';
+//import { AddMedicine } from './components/admin/add-medicine/add-medicine.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    FormsModule,
-    HttpClientModule,
-    AddMedicine
-  ],
+  imports: [RouterOutlet, 
+	       CommonModule,
+	       HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyApp';
+  title = 'medicine-ordering-app';
 }
--- medicine-ordering-app/src/main.ts
+++ medicine-ordering-app/src/main.ts
@@ -1,6 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+  import { bootstrapApplication } from '@angular/platform-browser';
+  import { AppComponent } from './app/app.component';
+  import { importProvidersFrom } from '@angular/core';
+  import { HttpClientModule } from '@angular/common/http';
+  import { RouterModule, Routes } from '@angular/router';
+
+  import { AddMedicine } from './app/components/admin/add-medicine/add-medicine.component';
+  //import { ManageMedicinesComponent } from './app/components/admin/manage-medicines/manage-medicines.component';
+
+  const routes: Routes = [
+    { path: '', redirectTo: 'manage-medicines', pathMatch: 'full' },
+    { path: 'add-medicine', component: AddMedicine },
+    //{ path: 'manage-medicines', component: ManageMedicinesComponent }
+  ];
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      importProvidersFrom(HttpClientModule, RouterModule.forRoot(routes))
+    ]
+  }).catch(err => console.error(err));
